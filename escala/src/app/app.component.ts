f80d942e390b6ce36995b1221c942cfdfe4b6144
Wesley <wes.mnovaes@gmail.com>
2024-04-15 17:49:54 -0300

    Finalizando layout e inicio da logica de serviços

--- escala/src/app/app.component.ts
+++ escala/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
+
 export class AppComponent {
-  title = 'Escala';
+
+  constructor(public datepipe: DatePipe){
+    let currentDateTime =this.datepipe.transform((new Date), 'MM');
+    console.log(currentDateTime);
+  }
+
+  pessoas = [{nome:"Maria",dias:['D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D']},
+             {nome:"Julia",dias:['D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D']},
+             {nome:"João",dias:['D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D']}
+            ]
 }
