db5f44d4fbec7142a8c64feac3b45d87fd624ee3
Wesley <wes.mnovaes@gmail.com>
2024-04-17 18:33:19 -0300

    Adiciona e configura biblioteca Moment

--- escala/src/app/app.component.ts
+++ escala/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DatePipe } from '@angular/common';
-import { EscalaService } from './escala.service'
+import { EscalaService } from './escala.service';
 
-import {Pipe, PipeTransform} from '@angular/core';
 import moment from '../../node_modules/moment';
+import '../../node_modules/moment/locale/pt-br';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  host: {ngSkipHydration: 'true'},
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
@@ -16,23 +16,16 @@ import moment from '../../node_modules/moment';
 
 export class AppComponent {
 
-  constructor(public datepipe: DatePipe, private $services: EscalaService){
-
-    let currentDateTime =this.datepipe.transform((new Date), 'EEEE, MMMM d, y');
-    console.log(currentDateTime);
+  constructor(private $services: EscalaService){ moment.locale('pt-br');
   }
+  
+  
+  a = moment().format('LLL');
 
+  
+  
   pessoas = [{nome:"Maria",dias:['D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D']},
              {nome:"Julia",dias:['D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D']},
              {nome:"João",dias:['D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D','-','D']}
-            ]
-}
-@Pipe({name: 'customDate'})
-export class CustomDatePipe implements PipeTransform {
-  transform(date, format = 'dd/MM/yyyy', dayOffset = 0, monthOffset = 0, yearOffset = 0) {
-    return moment(new Date(date)).add(dayOffset, 'days')
-                                 .add(monthOffset, 'months')
-                                 .add(yearOffset, 'years')
-                                 .format(format);
-  }
+            ]          
 }
